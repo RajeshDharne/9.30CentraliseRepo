a0999e6de548bceac64f83f722626c87554c0e96
Rajesh-pc <Rajesh-pc@192.168.1.7>
2020-08-08 18:44:30 +0530

    2nd code

---
+++ New_930_Batch/src/Constructor1/Methodusingthis.java
@@ -0,0 +1,23 @@
+package Constructor1;
+
+public class Methodusingthis 
+{
+	public void method1()
+	
+	{
+		
+		System.out.println("1 parameterized method");
+		
+	}
+	
+	public static void main(String[] args) 
+	
+	{
+		
+		Methodusingthis obj = new Methodusingthis();
+		obj.method1();
+		
+		
+	}
+
+}
